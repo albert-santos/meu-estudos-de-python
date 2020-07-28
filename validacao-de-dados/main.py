002c7f2e70eecae382fddf8e67d9c034045277b2
Albert <alberteinsteincds@gmail.com>
2020-07-28 12:11:04 -0300

    Utilizando Regex

--- validacao-de-dados/main.py
+++ validacao-de-dados/main.py
@@ -1,12 +1,37 @@
-from cpf_cnpj import Documento
-from validate_docbr import CNPJ
+from TelefonesBr import TelefonesBr
+import re
 
-cpf_um = "15316264754"
+#cpf_um = "15316264754"
 #print(cpf_um)
 
 #exemplo_cnpj = "35379838000112"
 #exemplo_cpf = "11"
 #cnpj = CNPJ()
 #print(cnpj.validate(exemplo_cnpj))
-documento = Documento.cria_documento(cpf_um)
-print(documento)
+#documento = Documento.cria_documento(cpf_um)
+#print(documento)
+
+'''
+padrao = "[0-9][a-z]{2}[0-9]"
+texto = "123 1aa2 342"
+resposta = re.search(padrao, texto)
+print(resposta.group())
+'''
+'''
+padrao = "\w{5,50}@\w{3,10}.\w{2,3}.\w{2,3}"
+texto = "alkodk albert123@gmail.com.br"
+resposta = re.search(padrao, texto)
+print(resposta.group())
+'''
+
+'''
+padrao_molde = "(xx)aaaa-www"
+padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+texto = "meus numeros são 91234568796 91987654857"
+resposta = re.findall(padrao, texto)
+print(resposta)
+'''
+
+telefone = "559163574985"
+telefone_objeto = TelefonesBr(telefone)
+print(telefone_objeto)
