fe72ceb6197a3b72015950b1681e862d6a5bc9b6
Albert <alberteinsteincds@gmail.com>
2020-07-28 15:53:05 -0300

    Respostas API Via CEP

--- validacao-de-dados/main.py
+++ validacao-de-dados/main.py
@@ -57,7 +57,5 @@ print(hoje.tempo_cadastro())
 
 cep = "01001000"
 objeto_cep = BuscaEndereco(cep)
-
-
-r = requests.get("https://viacep.com.br/ws/01001000/json/")
-print(r.text)
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro, cidade, uf)
