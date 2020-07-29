9bc17530be5a63336bf9823c2cd1b80423e226ae
Albert <alberteinsteincds@gmail.com>
2020-07-29 12:36:49 -0300

    Mais testes e classes de equivalência

--- tdd-com-python/src/leilao/test_avaliador.py
+++ tdd-com-python/src/leilao/test_avaliador.py
@@ -47,3 +47,43 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
+        gui = Usuario('Gui')
+        lance = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+        leilao.lances.append(lance)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        self.assertEqual(150.0, avaliador.maior_lance)
+        self.assertEqual(150.0, avaliador.menor_lance)
+
+
+    def test_deve_retornar_o_maior_e_menor_lance_quando_o_leilao_tiver_tres_lances(self):
+        gui = Usuario('Gui')
+        albert = Usuario('Albert')
+        vini = Usuario('Vini')
+
+        lance_do_albert = Lance(albert, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+        lance_do_vini = Lance(vini, 200)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_albert)
+        leilao.lances.append(lance_do_vini)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
