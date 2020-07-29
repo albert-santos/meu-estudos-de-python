41926f702e0d4d14b4690fd644aa42df2ae2e863
Albert <alberteinsteincds@gmail.com>
2020-07-29 12:13:43 -0300

    Começando testes com unidades

---
+++ tdd-com-python/src/leilao/test_avaliador.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+
+
+class TestAvaliador(TestCase):
+    def test_avalia(self):
+        gui = Usuario('Gui')
+        albert = Usuario('Albert')
+
+        lance_do_albert = Lance(albert, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_albert)
+        leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
