22b342e0835a5f2f02070adcba33fd6760e08306
Albert <alberteinsteincds@gmail.com>
2020-07-29 12:21:09 -0300

    Criando um novo teste

--- tdd-com-python/src/leilao/test_avaliador.py
+++ tdd-com-python/src/leilao/test_avaliador.py
@@ -25,3 +25,25 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_avalia2(self):
+        gui = Usuario('Gui')
+        albert = Usuario('Albert')
+
+        lance_do_albert = Lance(albert, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_albert)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
