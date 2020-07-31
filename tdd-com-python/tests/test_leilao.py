4fc87444bb1b45e1877dca1afe38b3e8c4aa1b5a
Albert <alberteinsteincds@gmail.com>
2020-07-31 11:28:02 -0300

    Primeiros testes com Pytest

--- tdd-com-python/tests/test_leilao.py
+++ tdd-com-python/tests/test_leilao.py
@@ -6,12 +6,12 @@ from src.leilao.dominio import Usuario, Lance, Leilao
 class TestLeilao(TestCase):
 
     def setUp(self):
-        self.gui = Usuario('Gui')
+        self.gui = Usuario('Gui', 500.0)
         self.lance_do_gui = Lance(self.gui, 150.0)
         self.leilao = Leilao('Celular')
 
     def test_retornar_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
-        albert = Usuario('Albert')
+        albert = Usuario('Albert', 500.0)
         lance_do_albert = Lance(albert, 100.0)
 
         self.leilao.propoe(lance_do_albert)
@@ -27,7 +27,7 @@ class TestLeilao(TestCase):
 
     def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
         with self.assertRaises(ValueError):
-            yuri = Usuario('Yuri')
+            yuri = Usuario('Yuri', 500.0)
             lance_do_yuri = Lance(yuri, 100.0)
 
             self.leilao.propoe(self.lance_do_gui)
@@ -42,10 +42,10 @@ class TestLeilao(TestCase):
 
 
     def test_deve_retornar_o_maior_e_menor_lance_quando_o_leilao_tiver_tres_lances(self):
-        albert = Usuario('Albert')
+        albert = Usuario('Albert', 500.0)
         lance_do_albert = Lance(albert, 100.0)
 
-        vini = Usuario('Vini')
+        vini = Usuario('Vini', 500.0)
         lance_do_vini = Lance(vini, 200)
 
 
@@ -67,7 +67,7 @@ class TestLeilao(TestCase):
         self.assertEqual(1, quantidade_de_lances_recebido)
 
     def test_deve_permitir_propor_um_lance_caso_o_ultimo_seja_diferente(self):
-        yuri = Usuario('Yuri')
+        yuri = Usuario('Yuri', 500.0)
         lance_do_yuri = Lance(yuri, 200.0)
 
         self.leilao.propoe(self.lance_do_gui)
